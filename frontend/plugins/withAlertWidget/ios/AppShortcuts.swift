fcfccf5e4b956bdc8d15015dad5a77e669152496
Lee Salminen <leesalminen@gmail.com>
2026-03-14 06:55:08 -0600

    build error

--- frontend/plugins/withAlertWidget/ios/AppShortcuts.swift
+++ frontend/plugins/withAlertWidget/ios/AppShortcuts.swift
@@ -3,18 +3,16 @@ import AppIntents
 @available(iOS 17.0, *)
 struct ElBotonAppShortcuts: AppShortcutsProvider {
     static var appShortcuts: [AppShortcut] {
-        [
-            AppShortcut(
-                intent: TriggerAlertIntent(),
-                phrases: [
-                    "Send alert with \(.applicationName)",
-                    "Trigger alert with \(.applicationName)",
-                    "Get help with \(.applicationName)"
-                ],
-                shortTitle: "Send Alert",
-                systemImageName: "exclamationmark.triangle.fill"
-            )
-        ]
+        AppShortcut(
+            intent: TriggerAlertIntent(),
+            phrases: [
+                "Send alert with \(.applicationName)",
+                "Trigger alert with \(.applicationName)",
+                "Get help with \(.applicationName)"
+            ],
+            shortTitle: "Send Alert",
+            systemImageName: "exclamationmark.triangle.fill"
+        )
     }
 
     static var shortcutTileColor: ShortcutTileColor = .red
